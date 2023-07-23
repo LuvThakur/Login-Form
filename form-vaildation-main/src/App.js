3cd80f592fc6b051f8573bad33036e2da8759c9c
Laijyourluv <laijyour26@gmail.com>
2023-07-23 13:28:35 +0530

    Login-form by celebal

---
+++ form-vaildation-main/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ValidationPage from './ValidationPage';
+
+
+const App = () => {
+  return (
+    <div>
+      <ValidationPage />
+    </div>
+  );
+};
+
+export default App;
